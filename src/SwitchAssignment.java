90cafb2c9b2747612cb7cfbf4e8f732aa86cff53
jlepler <jlepler@montgomerycollege.edu>
2019-06-13 16:58:27 -0400

    Initial commit of unfinished program SwitchAssignment

---
+++ src/SwitchAssignment.java
@@ -0,0 +1,24 @@
+/*This program will display the correct month(s) given the number of days in the month
+        as well as the number of the month given.*/
+
+import java.util.Scanner;
+
+public class SwitchAssignment {
+    public static void main(String [] args){
+
+        Scanner input = new Scanner(System.in);
+
+        int monthNumber;   //Declares variables for this program
+        String monthName;
+        int daysInMonth;
+
+
+        System.out.println("Please give the number of days of any month");
+        String daysInMonth = input.nextLine();
+        System.out.println("Please give the ");
+
+
+
+
+    }
+}
